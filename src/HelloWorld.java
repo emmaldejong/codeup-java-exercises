8694434e6f87b024ccb436ef8613698664848c0b
emmadejong <dejong.l.emma@gmail.com>
2018-02-27 14:14:07 -0600

    added ryan solution notes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,9 +11,17 @@ public class HelloWorld {
 
         double myNumber = 3.14;
         System.out.println(myNumber);
+        float number = (float)3.14;
+        System.out.println(number);
 
-        //questions about long vaiable, why could you put L in there?
+        //questions about long variable, why could you put L in there?
+//        longs don't hold decimals
+//        long is only hanging onto whole numbers
+//        if the number is out of the range of integers you put an L at the end
         //questions about how to fix float on the value side, just changed it to double because I know double is decimals
+//        double contains twice the available options than a float does, you can cast the (float) to 3.14 to be more specific and make the float variable work
+//          all floats are doubles, not all doubles are floats
+//          or you can put an F at the end of the number with float will make it work
 
         int z = 5;
         System.out.println(++z);
@@ -21,20 +29,20 @@ public class HelloWorld {
 
         //String class = "class"; class is a reserved word
 
-        int x = 4 + 5;
-        //x = x + 5;
+        int x = 4;
+        x += 5;
         System.out.println(x);
 
         int a = 3;
-        int y = 4 *  a;
-        //y = y * a;
+        int y = 4;
+        y *= a;
         System.out.println(y);
 
         int b = 10;
         int c = 2;
         // b = b / c;
         // c = c - b;
-        c = c - (b / c);
+        c -= (b / c);
         System.out.println(c);
     }
 }
