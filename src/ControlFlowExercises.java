554649f1c2478adc9aff2c55b662270ce073785b
emmadejong <dejong.l.emma@gmail.com>
2018-02-27 16:56:39 -0600

    working on do while loop section

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 2;
+//        do {
+//            System.out.println(i * i);
+//        } while (i < 1000000);
     }
 }
