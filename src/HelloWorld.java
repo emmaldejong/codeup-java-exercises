df82428bc901badaeb81a63b0d36b4a829419445
emmadejong <dejong.l.emma@gmail.com>
2018-02-27 12:01:21 -0600

    finished syntax and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!!");
-        System.out.println("Hello, Emma!!!!");
+//        System.out.println("Hello, World!!!!");
+//        System.out.println("Hello, Emma!!!!");
+
+        int myFavoriteNumber = 19;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Annie & DeeDee";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        //questions about long vaiable, why could you put L in there?
+        //questions about how to fix float on the value side, just changed it to double because I know double is decimals
+
+        int z = 5;
+        System.out.println(++z);
+        System.out.println(z);
+
+        //String class = "class"; class is a reserved word
+
+        int x = 4 + 5;
+        //x = x + 5;
+        System.out.println(x);
+
+        int a = 3;
+        int y = 4 *  a;
+        //y = y * a;
+        System.out.println(y);
+
+        int b = 10;
+        int c = 2;
+        // b = b / c;
+        // c = c - b;
+        c = c - (b / c);
+        System.out.println(c);
     }
 }
