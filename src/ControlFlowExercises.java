677e4a957bfbdc11be4d52473973e43f426a42c0
emmadejong <dejong.l.emma@gmail.com>
2018-02-27 20:46:25 -0600

    finished power table section

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //############# WHILE LOOPS #############
@@ -27,17 +29,47 @@ public class ControlFlowExercises {
 //        } while(i < 1000000);
 
 //########### FIZZ BUZZ ##################
-        for (int i = 1; i <= 100; i += 1) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
-        
+//        for (int i = 1; i <= 100; i += 1) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+//############# TABLE OF POWERS ################
+        Scanner scanner = new Scanner(System.in);
+        boolean confirmCont = true;
+
+            do {
+                System.out.print("Please enter a number: ");
+                int input = scanner.nextInt();
+
+                System.out.println("\nnumber | squared | cubed");
+                System.out.println("---- | ---- | ----");
+
+                for (int i = 1; i <= input; i += 1) {
+                    String output = "";
+                    output += String.format("%-7d", i);
+                    output += String.format("|%-9d", i * i);
+                    output += String.format("|%d", i * i * i);
+                    System.out.println(output);
+                }
+
+                System.out.print("\nDo you wish to continue? [y/n]");
+                String userChoice = scanner.next();
+                if (userChoice.equalsIgnoreCase("y") || userChoice.equalsIgnoreCase("yes")) {
+                    confirmCont = true;
+                } else {
+                    confirmCont = false;
+                }
+
+            } while (confirmCont);
+
+
     }
 }
