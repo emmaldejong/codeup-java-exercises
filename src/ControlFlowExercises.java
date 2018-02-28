c72a46c54fdc759b6f03033c4b91a70b9c22fd36
emmadejong <dejong.l.emma@gmail.com>
2018-02-28 12:30:03 -0600

    meow

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,7 +5,7 @@ public class ControlFlowExercises {
 //############# WHILE LOOPS #############
 //        int i = 5;
 //        while (i <= 15) {
-//            System.out.println(i);
+//            System.out.print(i + " ");
 //            i++;
 //        }
 
@@ -21,7 +21,7 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i -= 5;
 //        } while(i >= -10);
-
+//
 //        long i = 2;
 //        do {
 //            System.out.println(i);
@@ -139,22 +139,22 @@ public class ControlFlowExercises {
 //        } while (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
 
 // 2. Prompt the user for an integer. Output whether or not the number is prime.
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        do {
-            System.out.println("Enter any integer: ");
-            int numberInput = scanner.nextInt();
-
-            for (int i = 2; i < numberInput; i++) {
-                if (numberInput % i == 0) {
-                    System.out.println(numberInput + "is not Prime... :O");
-                } else {
-                    System.out.println(numberInput + "is Prime! :D");
-                }
-            }
-            System.out.println("Continue? [y/n");
-            input = scanner.next();
-        } while (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+//        Scanner scanner = new Scanner(System.in);
+//        String input;
+//        do {
+//            System.out.println("Enter any integer: ");
+//            int numberInput = scanner.nextInt();
+//
+//            for (int i = 2; i < numberInput; i++) {
+//                if (numberInput % i == 0) {
+//                    System.out.println(numberInput + "is not Prime... :O");
+//                } else {
+//                    System.out.println(numberInput + "is Prime! :D");
+//                }
+//            }
+//            System.out.println("Continue? [y/n");
+//            input = scanner.next();
+//        } while (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
 
 // 3. Prompt the user to enter an integer and output all the days of the week that
 //have no more letters than the integer given.
