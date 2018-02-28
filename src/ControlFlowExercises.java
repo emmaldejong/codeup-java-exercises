c6d10241160472428ce9295ee2b19b0447a89e66
emmadejong <dejong.l.emma@gmail.com>
2018-02-27 20:40:26 -0600

    finished FizzBuzz section

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -25,5 +25,19 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i *= i;
 //        } while(i < 1000000);
+
+//########### FIZZ BUZZ ##################
+        for (int i = 1; i <= 100; i += 1) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+        
     }
 }
