cba88f75d3f936663ca642e887cf3ce5cde37728
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 11:08:50 -0600

    added solution notes

--- src/BurgerTools.java
+++ src/BurgerTools.java
@@ -6,6 +6,6 @@ public class BurgerTools {
 
     public static void grill() {
         System.out.println( "grilling burger");
-//  adding static after public takes away the need to instance the object in the main method so that these can be called
+//  adding static after public takes away the need to instaniating the object in the main method so that these can be called
     }
 }
