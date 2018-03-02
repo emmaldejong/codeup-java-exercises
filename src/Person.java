b6cd173b51d04e2a0a9d1e8853c4a7cbc40a54a6
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 14:10:57 -0600

    added Person class with main method tests

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,11 @@
 public class Person {
-    private String name;
+    private static String name;
 
-    public void sayHello() {
+    public static void sayHello() {
         System.out.println("Say hello, " + name);
     }
 
+// ##### GETTER & SETTER
     public String getName() {
         return name;
     }
@@ -12,4 +13,24 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+// ##### CONSTRUCTOR
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+// #### MAIN METHOD
+
+    public static void main(String[] args) {
+        Person per = new Person("emma");
+
+        System.out.println(per.getName());
+//        System.out.println(per.setName("dwight"));
+
+        Person.sayHello();
+    }
+
+
 }
+
