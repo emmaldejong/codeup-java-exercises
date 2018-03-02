9906d26ca4ebdbd54e58aff9ac2fb2bfc870d75d
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 11:05:35 -0600

    added mini exercise 2

--- src/BurgerTester.java
+++ src/BurgerTester.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class BurgerTester {
+    public static void main(String[] args) {
+
+        BurgerTools.mostPopularTopping = "cheese";
+        BurgerTools.averageDaysBeforeExpiration = 2;
+        BurgerTools.temperatureWhenCooked = 280;
+
+        BurgerTools.grill();
+    }
 }
--- src/BurgerTools.java
+++ src/BurgerTools.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class BurgerTools {
+
+    public static String mostPopularTopping;
+    public static int averageDaysBeforeExpiration;
+    public static int temperatureWhenCooked;
+
+    public static void grill() {
+        System.out.println( "grilling burger");
+//  adding static after public takes away the need to instance the object in the main method so that these can be called
+    }
 }
