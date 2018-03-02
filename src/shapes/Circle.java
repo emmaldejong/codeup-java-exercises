c322e210b030a7544b9354bee90fca186559b7e9
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 16:31:49 -0600

    init commit and added initial constructor and methods

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,17 @@
 package shapes;
 
 public class Circle {
+   private double radius;
+
+// ##### CONSTRUCTOR
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(double radius) {
+        return (Math.PI * (radius * radius));
+    }
+    public double getCircumference(double radius) {
+        return 2 * Math.PI * radius;
+    }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,7 @@
 package shapes;
 
 public class CircleApp {
+    public static void main(String[] args) {
+
+    }
 }
