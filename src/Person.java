33fb4e851d21aacd714acca382e208702049e851
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 14:20:07 -0600

    added more code blocks to test methods in Person class for exercise 2

--- src/Person.java
+++ src/Person.java
@@ -15,20 +15,41 @@ public class Person {
     }
 
 // ##### CONSTRUCTOR
-
     public Person(String name) {
         this.name = name;
     }
 
 // #### MAIN METHOD
-
     public static void main(String[] args) {
-        Person per = new Person("emma");
+//        Person per = new Person("emma");
+//
+//        System.out.println(per.getName());
+//        per.setName("dwight");
+//        System.out.println(per.getName());
+//
+//        Person.sayHello();
 
-        System.out.println(per.getName());
-//        System.out.println(per.setName("dwight"));
+// ####### Exercise #2 - adding code block
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        returns true b/c they used the .equals method
+//        System.out.println(person1 == person2);
+//        returns false b;c they used == to compare the strings
 
-        Person.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        returned true, but why did it return true, in the end person1 and person2 are strings???
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        you set person1 equal to person2 and then and then get name, you changed the person's name to jane and so both get names will out put the new name b/c you set them equal
     }
 
 
