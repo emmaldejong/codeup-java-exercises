7bd25448c308b444dcf826151f8325d9034abfcc
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 12:25:38 -0600

    refactored for mini exercise 3

--- src/RestaurantDish.java
+++ src/RestaurantDish.java
@@ -1,13 +1,53 @@
 public class RestaurantDish {
 
 //  instance fields(properties and methods)
-    public int costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
 
     public String eat () {
         return "Nom nom nom";
 
-//  static
+    }
+//GETTER AND SETTER
+    public int getCostInCents() {
+        return costInCents;
+    }
+
+    public void setCostInCents(int costInCents) {
+        this.costInCents = costInCents;
+    }
+
+    public String getNameOfDish() {
+        return nameOfDish;
+    }
+
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    public boolean isWouldRecommend() {
+        return wouldRecommend;
+    }
+
+    public void setWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
+    }
+
+//    CONSTRUCTORS
+//    public RestaurantDish(int cost) {
+//        costInCents = cost;
+//    }
+//    public RestaurantDish(String name) {
+//        nameOfDish = name;
+//    }
+//    public RestaurantDish(boolean recommend) {
+//        wouldRecommend = recommend;
+//    }
+
+    public RestaurantDish(int costInCents, String nameOfDish, boolean wouldRecommend) {
+        this.costInCents = costInCents;
+        this.nameOfDish = nameOfDish;
+        this.wouldRecommend = wouldRecommend;
     }
 }
--- src/RestaurantTest.java
+++ src/RestaurantTest.java
@@ -1,10 +1,20 @@
 public class RestaurantTest {
     public static void main(String[] args) {
-        RestaurantDish dish = new RestaurantDish();
+        RestaurantDish dish = new RestaurantDish(20, "Pasta", true);
 
-        dish.costInCents = 20;
-        dish.nameOfDish = "Pasta";
-        dish.wouldRecommend = true;
+//        dish.costInCents = 20;
+//        dish.nameOfDish = "Pasta";
+//        dish.wouldRecommend = true;
         System.out.println(dish.eat());
+
+//        dish.setCostInCents(20);
+        System.out.println(dish.getCostInCents());
+
+//        dish.setNameOfDish("Pasta");
+        System.out.println(dish.getNameOfDish());
+
+//        dish.setWouldRecommend(true);
+        System.out.println(dish.isWouldRecommend());
+
     }
 }
