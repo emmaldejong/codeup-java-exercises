fce75ca028fdcbe54a1742ecaf5e25611710fbf6
emmadejong <dejong.l.emma@gmail.com>
2018-03-02 17:02:35 -0600

    added Circle methods and construtor and added circle app

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -3,15 +3,22 @@ package shapes;
 public class Circle {
    private double radius;
 
+   private static int numberOfCircles = 0;
+
 // ##### CONSTRUCTOR
     public Circle(double radius) {
         this.radius = radius;
+        numberOfCircles++;
     }
 
     public double getArea(double radius) {
-        return (Math.PI * (radius * radius));
+        return Math.PI * (radius * radius);
     }
     public double getCircumference(double radius) {
         return 2 * Math.PI * radius;
+    }
+
+    public static int getNumberOfCircles () {
+        return numberOfCircles;
     }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,6 +2,9 @@ package shapes;
 
 public class CircleApp {
     public static void main(String[] args) {
+        System.out.println("how many circles right now");
+        System.out.println(Circle.getNumberOfCircles());
 
+        Circle one = new Circle(1);
     }
 }
