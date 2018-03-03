4a54daac1c6438336d06b843a12ddf77de1653ca
emmadejong <dejong.l.emma@gmail.com>
2018-03-03 11:41:09 -0600

    added refactored to use ryan solutions

--- src/util/Input.java
+++ src/util/Input.java
@@ -10,51 +10,119 @@ public class Input {
         this.scan = new Scanner(System.in);
     }
 
-    public String getString(String prompt) {
-        System.out.println(prompt);
-        String userInput = scan.nextLine();
-        return userInput;
+//############## refactored to ryan's code - simplified to reuse instead of write scan.nextLine in every problem
+    public String getString () {
+        return scan.next();
+    }
+
+//    this was my original code that is hard to reuse throughout an application because I'm specifying that the argument has to be a string
+//    public String getString(String prompt) {
+//        System.out.println(prompt);
+//        String userInput = scan.nextLine();
+//        return userInput;
+//    }
+
+//############### refactored ryan's code to separate the yesNo() method and overload it, this helps to reuse the first yesNo method to other functions that might need to ask a boolean yesNo question
+    public boolean yesNo() {
+        String userInput = getString();
+        if(userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean yesNo(String prompt) {
         System.out.println(prompt);
-        String answer = scan.nextLine();
-        return (answer.toLowerCase().startsWith("y"));
+        return yesNo();
+    }
+//
+//    public boolean yesNo(String prompt) {
+//        System.out.println(prompt);
+//        String answer = scan.nextLine();
+//        return (answer.toLowerCase().startsWith("y"));
+//    }
+
+// ############# refactored to ryan's code to separate the getInt method into several simplified and reusable methods that call on each other to preform different tasks
+    public int getInt() {
+        return scan.nextInt();
     }
 
     public int getInt(String prompt) {
         System.out.println(prompt);
-        int integer = scan.nextInt();
-        return integer;
+        return getInt();
     }
 
-    public int getIntWithinRange(String prompt, int min, int max) {
-        int intInput = getInt(prompt);
-
+    public int getInt(int min, int max) {
+        int intInput = getInt();
         if(intInput < min || intInput > max) {
-            System.out.println("Number is out of range between " + min + " and " + max + ".");
-            return getIntWithinRange(prompt, min, max);
+            return getInt(min, max);
         } else {
-            System.out.println("Your number is within range!");
+            return intInput;
         }
-        return intInput;
+    }
+
+    public int getInt(int min, int max, String prompt){
+        System.out.println(prompt);
+        return getInt(min, max);
+    }
+
+//    public int getInt(String prompt) {
+//        System.out.println(prompt);
+//        int integer = scan.nextInt();
+//        return integer;
+//    }
+
+//    public int getIntWithinRange(String prompt, int min, int max) {
+//        int intInput = getInt(prompt);
+//
+//        if(intInput < min || intInput > max) {
+//            System.out.println("Number is out of range between " + min + " and " + max + ".");
+//            return getIntWithinRange(prompt, min, max);
+//        } else {
+//            System.out.println("Your number is within range!");
+//        }
+//        return intInput;
+//    }
+
+// ############# ryan's code just like the getInt methods to make then reusable and call on each other to build their functionality
+    public double getDouble() {
+        return scan.nextDouble();
     }
 
     public double getDouble(String prompt) {
         System.out.println(prompt);
-        double answer = scan.nextDouble();
-        return answer;
+        return getDouble();
     }
 
-    public double getDoubleWithinRange(String prompt, double min, double max) {
-        double doubleInput = getDouble(prompt);
-
-        if(doubleInput < min || doubleInput > max) {
-            System.out.println("Your decimal is not within range of " + min + " and " + max + ".");
-            return getDoubleWithinRange(prompt, min, max);
+    public double getDouble(double min, double max) {
+        double intDouble = getDouble();
+        if(intDouble < min || intDouble > max) {
+            return getDouble(min, max);
         } else {
-            System.out.println("Your decimal is within range!");
+            return intDouble;
         }
-        return doubleInput;
     }
+
+    public double getDouble(double min, double max, String prompt) {
+        System.out.println(prompt);
+        return getDouble(min, max);
+    }
+//    public double getDouble(String prompt) {
+//        System.out.println(prompt);
+//        double answer = scan.nextDouble();
+//        return answer;
+//    }
+//
+//    public double getDoubleWithinRange(String prompt, double min, double max) {
+//        double doubleInput = getDouble(prompt);
+//
+//        if(doubleInput < min || doubleInput > max) {
+//            System.out.println("Your decimal is not within range of " + min + " and " + max + ".");
+//            return getDoubleWithinRange(prompt, min, max);
+//        } else {
+//            System.out.println("Your decimal is within range!");
+//        }
+//        return doubleInput;
+//    }
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,16 +3,44 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
         Input input = new Input();
-        System.out.println(input.getString("How's your day?"));
 
-        System.out.println(input.yesNo("Do you like cats?"));
+        int num1 = input.getInt(1, 10, "Enter a number between 1 & 10:");
 
-        System.out.println(input.getInt("Enter a number..."));
+        System.out.println("You've entered the number " + num1);
 
-        System.out.println(input.getIntWithinRange("Enter another number...", 0, 20));
+        System.out.println("Do you want to enter another number? [y/n]");
 
-        System.out.println(input.getDouble("Enter a decimal number..."));
+        boolean choice = input.yesNo();
 
-        System.out.println(input.getDoubleWithinRange("Enter another decimal...", 0, 6));
+        System.out.println();
+
+        if(choice) {
+            int num2 = input.getInt("Enter any valid integer");
+            System.out.println("You've entered " + num2);
+        }
+
+        if(choice) {
+            int num3 = input.getInt("Wanna enter another integer?");
+            System.out.println("You've entered " + num3);
+        }
+
+        double someDouble = input.getDouble("Enter an integer:");
+        System.out.println("You entered " + someDouble);
+
+        double doubleInRange = input.getDouble(1.1, 9.9, "Input any number between 1.1 & 9.9:");
+        System.out.println(doubleInRange);
+
+
+//        System.out.println(input.getString("How's your day?"));
+
+//        System.out.println(input.yesNo("Do you like cats?"));
+
+//        System.out.println(input.getInt("Enter a number..."));
+
+//        System.out.println(input.getIntWithinRange("Enter another number...", 0, 20));
+
+//        System.out.println(input.getDouble("Enter a decimal number..."));
+
+//        System.out.println(input.getDoubleWithinRange("Enter another decimal...", 0, 6));
     }
 }
