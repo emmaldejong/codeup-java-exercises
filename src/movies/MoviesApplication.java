9b66d403c1d4f091ac458892ee70b651863a058f
emmadejong <dejong.l.emma@gmail.com>
2018-03-05 16:48:13 -0600

    added arraylist for add movie functionality

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -2,41 +2,63 @@ package movies;
 
 import util.Input;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MoviesApplication {
+
+    static Movie[] viewAll = MoviesArray.findAll();
 
     public static void main(String[] args) {
         Input input = new Input();
 
         do {
-            int userInput = input.getInt(0, 5, "What would you like to do?\n" +
-                    "0 - exit\n" +
+            int userInput = input.getInt(0, 6, "What would you like to do?\n" +
+//                    "0 - exit\n" +
                     "1 - view all movies\n" +
                     "2 - view movies in the animated category\n" +
                     "3 - view movies in the drama category\n" +
                     "4 - view movies in the horror category\n" +
                     "5 - view movies in the scifi category\n" +
+                    "6 - add a new movie\n" +
                     "Enter your choice: ");
 
             displaySelection(userInput);
+
             System.out.println("===========================================================");
-        } while (input.yesNo("Would you like to continue?"));
+
+        } while (input.yesNo("Would you like to make another selection or add a movie?"));
+
         System.out.println("Goodbye!");
+
+//        String[] thisIsAStringArray = {"Car", "Van", "Bike"};
+//        MoviesArray = add(MoviesArray, "SUV");
+//        for (String element:MoviesArray) {
+//            System.out.println( element );
+//        }
     }
 
     public static void viewAllMovies() {
-        Movie[] viewAll = MoviesArray.findAll();
         for (Movie movie : viewAll) {
             System.out.println(movie.getName() + " -- " + movie.getCategory());
         }
     }
 
     public static void viewByCategory(String category) {
-        Movie[] viewAll = MoviesArray.findAll();
         for (Movie movie : viewAll) {
             if (category.equalsIgnoreCase(movie.getCategory())) {
                 System.out.println(movie.getName() + " -- " + movie.getCategory());
             }
         }
+    }
+    public static void addMovie() {
+        Input input = new Input();
+        String newMovieName = input.getString("Enter the new movie's name:");
+        String newMovieCategory = input.getString("Enter the new movie's category:");
+        Movie userNewMovie = new Movie(newMovieName, newMovieCategory);
+//        Movie[] moviesArray = Arrays.copyOf(viewAll, viewAll.length + 1);
+        ArrayList<Movie> moviesArray = new ArrayList<>();
+        moviesArray.add(userNewMovie);
     }
 
     public static void displaySelection(int userInput) {
@@ -57,11 +79,16 @@ public class MoviesApplication {
             case 5:
                 viewByCategory("scifi");
                 break;
+            case 6:
+                addMovie();
+                break;
             default:
                 System.out.println("Goodbye!");
                 break;
         }
     }
+
+
 }
 
 
