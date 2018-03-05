27cf97758c7e2bb597feeb5ac7821c9aae5e2da9
emmadejong <dejong.l.emma@gmail.com>
2018-03-05 11:05:49 -0600

    init commit and added constructor and getter setters to Movie.java

---
+++ src/movies/Movie.java
@@ -0,0 +1,36 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    private static int numberOfMovies = 0;
+
+//############ CONSTRUCTOR
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+        numberOfMovies++;
+    }
+
+//############ GETTER & SETTER
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public static int getNumberOfMovies() {
+        return numberOfMovies;
+    }
+}
