ef8a8794d32c430b421fbac1060470c0c16db4f7
emmadejong <dejong.l.emma@gmail.com>
2018-03-05 14:24:29 -0600

    finished main guts of application and testing

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -4,35 +4,44 @@ import util.Input;
 
 public class MoviesApplication {
 
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        do {
+            int userInput = input.getInt(0, 5, "What would you like to do?\n" +
+                    "0 - exit\n" +
+                    "1 - view all movies\n" +
+                    "2 - view movies in the animated category\n" +
+                    "3 - view movies in the drama category\n" +
+                    "4 - view movies in the horror category\n" +
+                    "5 - view movies in the scifi category\n" +
+                    "Enter your choice: ");
+
+            displaySelection(userInput);
+            System.out.println("===========================================================");
+        } while (input.yesNo("Would you like to continue?"));
+        System.out.println("Goodbye!");
+    }
+
     public static void viewAllMovies() {
         Movie[] viewAll = MoviesArray.findAll();
-        for(Movie movie : viewAll) {
+        for (Movie movie : viewAll) {
             System.out.println(movie.getName() + " -- " + movie.getCategory());
         }
     }
 
     public static void viewByCategory(String category) {
         Movie[] viewAll = MoviesArray.findAll();
-        for(Movie movie : viewAll) {
-            if(category.equalsIgnoreCase(movie.getCategory())) {
+        for (Movie movie : viewAll) {
+            if (category.equalsIgnoreCase(movie.getCategory())) {
                 System.out.println(movie.getName() + " -- " + movie.getCategory());
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void displaySelection(int userInput) {
 
-        Input input = new Input();
-        int choice = input.getInt("What would you like to do?\n" +
-                "0 - exit\n" +
-                "1 - view all movies\n" +
-                "2 - view movies in the animated category\n" +
-                "3 - view movies in the drama category\n" +
-                "4 - view movies in the horror category\n" +
-                "5 - view movies in the scifi category\n" +
-                "Enter your choice: ");
-
-        switch (choice) {
+        switch (userInput) {
             case 1:
                 viewAllMovies();
                 break;
@@ -53,7 +62,9 @@ public class MoviesApplication {
                 break;
         }
     }
- }
+}
+
+
 
 
 
