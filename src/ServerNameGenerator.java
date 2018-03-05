6aa41d2322930e66b63dbddc09e7618f03c94834
emmadejong <dejong.l.emma@gmail.com>
2018-03-05 10:45:18 -0600

    init commit with two new arrays

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,29 @@
+
+public class ServerNameGenerator {
+
+    public static void main(String[] args) {
+        String[] nouns = new String[10];
+        nouns[0] = "moon";
+        nouns[1] = "space";
+        nouns[2] = "black hole";
+        nouns[3] = "galaxy";
+        nouns[4] = "antimatter";
+        nouns[5] = "orb";
+        nouns[6] = "meteor";
+        nouns[7] = "star";
+        nouns[8] = "asteriod";
+        nouns[9] = "exoplanet";
+
+        String[] adjectives = new String[10];
+        adjectives[0] = "fierce";
+        adjectives[1] = "jolly";
+        adjectives[2] = "ambitious";
+        adjectives[3] = "witty";
+        adjectives[4] = "microscopic";
+        adjectives[5] = "massive";
+        adjectives[6] = "cloudy";
+        adjectives[7] = "dangerous";
+        adjectives[8] = "evil";
+        adjectives[9] = "stormy";
+    }
+}
