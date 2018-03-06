a3a6739f3b4cb6263387d6fdcdde7494b42e51d5
emmadejong <dejong.l.emma@gmail.com>
2018-03-06 14:52:42 -0600

    added square class and side constructor that extends rectangle

---
+++ src/shapes/Square.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class Square extends Rectangle{
+    private int side;
+
+    public Square(int length, int width, int side) {
+        super(length, width);
+        this.side = side;
+    }
+}
