206e042fb7630ab320c934cfa75bee588c32a23a
emmadejong <dejong.l.emma@gmail.com>
2018-03-08 12:02:32 -0600

    completed application in console for userinput

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,9 +1,12 @@
 package grades;
 
+import util.Input;
+
 import java.util.HashMap;
 
 public class GradesApplication {
     public static void main(String[] args) {
+        Input input = new Input();
         HashMap<String, Student> students = new HashMap<>();
 
         Student ryan = new Student("Ryan");
@@ -30,5 +33,30 @@ public class GradesApplication {
         students.put("jreich5", justin);
         students.put("zgulde", zach);
         students.put("jordanisawesome", jordan);
+
+        System.out.println("Welcome!");
+        System.out.println("============================================");
+        System.out.println("Here are the github usernames of our students: \n" +
+                students.keySet()
+        );
+        System.out.println("============================================");
+
+        boolean willContinue;
+        do {
+            String usernameInput = input.getString("What student would you like to see more information on?");
+
+            if(students.containsKey(usernameInput)) {
+                System.out.println(students.get(usernameInput).getStudentName() + " has the average grade of " + students.get(usernameInput).getGradeAverage());
+            } else {
+                System.out.println("Sorry, no student found with the GitHub username of \"" + usernameInput + "\".");
+            }
+
+            System.out.println("============================================");
+            System.out.println();
+            willContinue = input.yesNo("Would you like to view another student? [Y/N]");
+
+        } while (willContinue);
+
+        System.out.println("Goodbye!");
     }
 }
