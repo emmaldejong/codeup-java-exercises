bee7c7bfa9435a3931eb8f6605b244d987c6abb1
emmadejong <dejong.l.emma@gmail.com>
2018-03-08 10:10:53 -0600

    added Student class with getStudentName and getAverage & added main method to test

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,5 +1,49 @@
 package grades;
 
-public class Student {
+import java.util.ArrayList;
 
+public class Student {
+    private String studentName;
+    private ArrayList<Integer> grades;
+
+    //CONSTRUCTOR
+    public Student(String studentName) {
+        this.studentName = studentName;
+        grades = new ArrayList<>();
+    }
+
+    // RETURN'S THE STUDENT'S NAME
+    public String getStudentName() {
+        return studentName;
+    }
+
+    //ADDS THE GIVEN GRADE TO THE GRADES PROPERTY
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    //RETURNS THE AVERAGE OF THE STUDENTS GRADES
+    public double getGradeAverage() {
+        double sumOfAllGrades = 0;
+
+        for(int grade : grades) {
+            sumOfAllGrades += grade;
+        }
+
+        double average = sumOfAllGrades / grades.size();
+        return average;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Emma");
+
+        student.grades.add(86);
+        student.grades.add(88);
+        student.grades.add(90);
+
+        System.out.println("Here are " + student.getStudentName() + "'s current grades: " + student.grades);
+
+        System.out.println("Here is the average of " + student.getStudentName() + "'s average grade: " + student.getGradeAverage());
+
+    }
 }
