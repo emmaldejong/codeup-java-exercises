419fce06fd81ba45b666e220bd43f034d98685e2
emmadejong <dejong.l.emma@gmail.com>
2018-03-14 09:53:58 -0500

    added study notes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-//        System.out.println("Hello, World!!!!");
-//        System.out.println("Hello, Emma!!!!");
+        System.out.println("Hello, World!!!!");
+        System.out.println("Hello, Emma!!!!");
 
         int myFavoriteNumber = 19;
         System.out.println(myFavoriteNumber);
